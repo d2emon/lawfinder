1fffecd906dc6b98f922d32b8c0eadeb7293d8ca
Dmitry Kutsenko <d2emonium@gmail.com>
2016-12-07 19:53:36 +0300

    Search div

--- run.py
+++ run.py
@@ -18,11 +18,13 @@ def main():
     buf.close()
 
     body = re.compile(r"<\s*/?\s*body\s*>")
-
     tag = re.compile("<(.*)")
+    # doc = re.compile(r"\s*<div class=\"list-docs-i?!>*>")
+    doc = re.compile(r"<div\s*class=\"list-docs-*>")
 
     parts = body.split(content)
-    main_part = parts[1]
+    main_part = parts[1].decode('cp1251').encode('utf8')
+    doclist = doc.split(main_part)
 
     for s in content:
         print("--->\t" + s)
@@ -32,6 +34,11 @@ def main():
 
     print("="*80)
     print(main_part)
+
+    print("="*80)
+    for d in doclist:
+        print(d)
+        print('_' * 80)
 
 
 if __name__ == "__main__":
